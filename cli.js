8a7e8547cca64c86867ca31e8501e072f8d7a8f8
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-08-31 10:03:43 -0500

    feat: initial commit for cli.js

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const CodeSimplicityToolkit = require('./index');
+const toolkit = new CodeSimplicityToolkit();
+
+const args = process.argv.slice(2);
+if (args[0] === 'analyze') {
+    const filePath = args[1];
+    toolkit.analyze(filePath);
+} else {
+    console.log('Command not recognized.');
+}
